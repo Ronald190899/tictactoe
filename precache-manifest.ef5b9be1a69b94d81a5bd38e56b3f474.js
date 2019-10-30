d14a56560e51b101c41c6deba8c3244875cc3b66
Ronald190899 <ronald190899@gmail.com>
2019-10-30 11:42:59 -0600

    Updates

---
+++ precache-manifest.ef5b9be1a69b94d81a5bd38e56b3f474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c47006e7d25286b788828184f8f59aa",
+    "url": "Ronald190899.github.io/tictactoe/index.html"
+  },
+  {
+    "revision": "eac75cc3e78a01b0cfbb",
+    "url": "Ronald190899.github.io/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "Ronald190899.github.io/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "eac75cc3e78a01b0cfbb",
+    "url": "Ronald190899.github.io/tictactoe/static/js/main.0ac6f443.chunk.js"
+  },
+  {
+    "revision": "736b1a74e9144b9d790f",
+    "url": "Ronald190899.github.io/tictactoe/static/js/runtime-main.1069ddcc.js"
+  }
+]);
